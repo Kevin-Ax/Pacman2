d386b4597ecebbfac0134305c469f4000a82cab6
Kevin de Castro <kevinlorecine@gmail.com>
2022-03-28 12:19:51 -0300

    video 16, seguir do 17 em diante

--- app_class.py
+++ app_class.py
@@ -1,9 +1,15 @@
+import sys
 from ctypes.wintypes import HACCEL
 from tkinter import Widget
 from turtle import width
-import pygame, sys
-from settings import *
-from player_class import *
+
+import pygame
+
+from player_class import Player
+from enemy_class import *
+from settings import (BLACK, FPS, GREY, HEIGHT, MAP_HEIGHT, MAP_WIDTH,
+                      START_FONT, START_TEXT_SIZE, TOP_BOTTOM_BUFFER, WHITE,
+                      WIDTH)
 
 pygame.init()
 vec = pygame.math.Vector2
@@ -16,11 +22,17 @@ class App:
         self.state = 'start'
         self.cell_width = MAP_WIDTH//28
         self.cell_height = MAP_HEIGHT//30
-        self.player = Player(self, PLAYER_START_POS)
         self.walls = []
-        
+        self.coins = []
+        self.enemies = []
+        self.e_pos = []
+        self.p_pos = None
+
         self.load()
-        
+
+        self.player = Player(self, self.p_pos)
+        self.make_enemies()
+
     def run(self):
         while self.running:
             if self.state == 'start':
@@ -37,7 +49,7 @@ class App:
         pygame.quit()
         sys.exit()
 
-####################################  funções de apoio  #############################################            
+####################################  funções de apoio  #############################################
     def draw_text(self, words, screen, pos, size, colour, font_name, center=False):
         font = pygame.font.SysFont(font_name, size)
         text = font.render(words, False, colour)
@@ -46,18 +58,31 @@ class App:
             pos[0] = pos[0]-text_size[0]//2
             pos[1] = pos[1]-text_size[1]//2
         screen.blit(text, pos)
-        
+
     def load(self):
         self.background = pygame.image.load('map.png')
         self.background = pygame.transform.scale(self.background, (MAP_WIDTH, MAP_HEIGHT))
-        
+
         #abrindo arwuivo das paredes e criando uma lista com suas cordenadas
         with open("walls.txt", 'r') as file:
             for yidx, line in enumerate(file):
                 for xidx, char in enumerate(line):
                     if char == '1':
                         self.walls.append(vec(xidx,yidx))
+                    elif char == 'C':
+                        self.coins.append(vec(xidx,yidx))
+                    elif char == 'P':
+                        self.p_pos = vec(xidx,yidx)
+                    elif char in ["2","3","4","5"]:
+                        self.e_pos.append(vec(xidx, yidx))
+                    elif char == "B":
+                        pygame.draw.rect(self.background, BLACK, (xidx*self.cell_width, yidx*self.cell_height,
+                                        self.cell_width, self.cell_height))
                         
+                        
+    def make_enemies(self):
+        for idx, pos in enumerate(self.e_pos):
+            self.enemies.append(Enemy(self, pos, idx))
         
     def draw_grid(self):
         for x in range(WIDTH//self.cell_width):
@@ -66,8 +91,8 @@ class App:
             pygame.draw.line(self.background, GREY, (0, x*self.cell_height), (WIDTH, x*self.cell_height))
         #for wall in self.walls:
          #   pygame.draw.rect(self.background, RED, (wall.x*self.cell_width, wall.y*self.cell_height, self.cell_width, self.cell_height))
-        
-####################################  eventos de incio  ##############################################            
+
+####################################  eventos de incio  ##############################################
 
     def start_events(self):
         for event in pygame.event.get():
@@ -75,19 +100,18 @@ class App:
                 self.running = False
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 self.state = 'playing'
-                
+
     def start_update(self):
         pass
-    
-        
+
     def start_draw(self):
         self.screen.fill(BLACK)
         self.draw_text('PRESSIONE A BARRA DE ESPAÇO', self.screen, [WIDTH//2, HEIGHT//2-60], START_TEXT_SIZE, (170,132,58), START_FONT, center=True)
         self.draw_text('1 JOGADOR APENAS', self.screen, [WIDTH//2, HEIGHT//2+65], START_TEXT_SIZE, (33,137,156), START_FONT, center=True)
         self.draw_text('Maior pontuação', self.screen, [4,0], START_TEXT_SIZE, (255,255,255), START_FONT)
         pygame.display.update()
-        
-####################################  eventos jogaveis  ##############################################            
+
+####################################  eventos jogaveis  ##############################################
 
     def playing_events(self):
         for event in pygame.event.get():
@@ -103,17 +127,31 @@ class App:
                 if event.key == pygame.K_RIGHT:
                     self.player.move(vec(1,0))
 
-                    
-                
     def playing_update(self):
         self.player.update()
-    
-        
+        for enemy in self.enemies:
+            enemy.update()
+
     def playing_draw(self):
         self.screen.fill(BLACK)
         self.screen.blit(self.background, (TOP_BOTTOM_BUFFER//2, TOP_BOTTOM_BUFFER//2))
-        #self.draw_grid()
-        self.draw_text('Pontuação atual: 0', self.screen, [60,0], 18, WHITE, START_FONT, center=False)
+        self.draw_coins()
+        # self.draw_grid()
+        self.draw_text(f'Pontuação atual: {self.player.current_score}', self.screen, [60,0], 18, WHITE, START_FONT, center=False)
         self.draw_text('Maior Pontuação: 0', self.screen, [WIDTH//2+60,0], 18, WHITE, START_FONT, center=False)
         self.player.draw()
+        for enemy in self.enemies:
+            enemy.draw()
         pygame.display.update()
+
+    def draw_coins(self):
+        for coin in self.coins:
+            pygame.draw.circle(
+                self.screen,
+                (124, 123, 7),
+                (
+                    int(coin.x * self.cell_width) + self.cell_width // 2 + TOP_BOTTOM_BUFFER // 2,
+                    int(coin.y * self.cell_height) + self.cell_height // 2 + TOP_BOTTOM_BUFFER // 2,
+                ),
+                5
+            )
