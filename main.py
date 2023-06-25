feb3bf165d2a647b1ddb4083ea42183cd3b19649
Kevin Alexandre de Castro <devkevincl@gmail.com>
2023-06-25 15:22:46 -0300

    fixing main branch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 """
 O Jogo foi inspirado e feito seguindo o tutorial disponivel no seguinte link https://www.youtube.com/playlist?list=PLryDJVmh-ww3AMl8NSjp9YygWWTOfePu7
 """
-
 from app_class import App
 
 if __name__ == '__main__':
